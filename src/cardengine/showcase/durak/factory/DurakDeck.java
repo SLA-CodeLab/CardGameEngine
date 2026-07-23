c734befde130a60c63fa06cff553be444d619a14
Stanislav <stas1.17.stas1@gmail.com>
2026-07-23 13:16:25 +0200

    FactroryPattern zum Deckbauen ist bei DurakDeck fertig

--- src/cardengine/showcase/durak/factory/DurakDeck.java
+++ src/cardengine/showcase/durak/factory/DurakDeck.java
@@ -1,5 +1,7 @@
 package cardengine.showcase.durak.factory;
 
+import cardengine.framework.core.Rank;
+import cardengine.framework.core.SimpleCard;
 import cardengine.framework.core.StandardDeck;
 import cardengine.framework.core.Suit;
 
@@ -7,12 +9,28 @@ public class DurakDeck extends StandardDeck {
     /**
      * @author Stanislav
      */
+    private final static int DURAK_RANK_COUNT = 9;
+
     private Suit trumpSuit;
 
     public DurakDeck() {
-        //ka
+        Rank[] ranks = Rank.values();// {TWO, THREE .. usw}
+        for (Suit suit : Suit.values()) { //{HEARTS,DIAMONDS,CLUBS,SPADES}
+            for (int i = getStartIndex(ranks); i < ranks.length; i++) {
+                addCard(new SimpleCard(suit, ranks[i]));
+            }
+        }
     }
+    /**
+     * @author Stanislav
+     */
+    //um eine Deck zu erzuegen, die mit 6 Startet, eigentlich koennte man auch bei Framework schreiben
+    // die loesung von Claude war bei MiniGame --   private static final int MIN_RANK_ORDINAL = Rank.SIX.ordinal();
 
+    private int getStartIndex(Rank[] ranks) {
+        int skip = Math.max(0, ranks.length - DURAK_RANK_COUNT);
+        return skip;
+    }
 
     public Suit getTrumpSuit() {
         return trumpSuit;
